3c5655234b2f8bb6a0671d5171557f806f121db7
AmandaTI241001010258 <2410010258manda@gmail.com>
2026-07-01 21:34:08 +0800

    Selesai service: DataProduk

---
+++ src/service/DataProduk.java
@@ -0,0 +1,66 @@
+package service;
+
+import model.Produk;
+
+public class DataProduk {
+
+    private Produk[] daftarProduk = new Produk[100];
+    private int jumlah = 0;
+
+    // Tambah produk
+    public void tambahProduk(Produk produk) {
+        if (jumlah < daftarProduk.length) {
+            daftarProduk[jumlah] = produk;
+            jumlah++;
+            System.out.println("Produk berhasil ditambahkan.");
+        } else {
+            System.out.println("Data produk penuh.");
+        }
+    }
+
+    // Tampilkan semua produk
+    public void tampilkanProduk() {
+        if (jumlah == 0) {
+            System.out.println("Belum ada data produk.");
+            return;
+        }
+
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("\nProduk ke-" + (i + 1));
+            daftarProduk[i].tampilkanData();
+        }
+    }
+
+    // Cari produk berdasarkan kode
+    public Produk cariProduk(String kode) {
+        for (int i = 0; i < jumlah; i++) {
+            if (daftarProduk[i].getKode().equalsIgnoreCase(kode)) {
+                return daftarProduk[i];
+            }
+        }
+        return null;
+    }
+
+    // Hapus produk
+    public boolean hapusProduk(String kode) {
+        for (int i = 0; i < jumlah; i++) {
+            if (daftarProduk[i].getKode().equalsIgnoreCase(kode)) {
+
+                for (int j = i; j < jumlah - 1; j++) {
+                    daftarProduk[j] = daftarProduk[j + 1];
+                }
+
+                daftarProduk[jumlah - 1] = null;
+                jumlah--;
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Getter jumlah data
+    public int getJumlah() {
+        return jumlah;
+    }
+}
